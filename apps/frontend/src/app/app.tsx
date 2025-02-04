42481262c007945e6dc084cbab7ac5c02b3c65a6
Jacob Fu <jjacobfu@gmail.com>
2025-02-04 12:09:49 -0500

    set up express backend, set up api routes for getting data

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,49 +1,37 @@
-import NxWelcome from './nx-welcome'
-
 import { Route, Routes, Link } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { getCompletion, getEmployee, getOpenTenderNotices } from '../api'
+
+interface Employee {
+  employee_id: number;
+  first_name: string;
+}
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="apps" />
+  const [message, setMessage] = useState('')
+  const [EmployeData, setEmployeeData] = useState<Employee[]>([])
 
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
-    </div>
+  useEffect(() => {
+    async function fetchMessage() {
+      // const data = await getCompletion()
+      const employeeResponse = await getEmployee()
+      setEmployeeData(employeeResponse.data);
+      // setMessage(data)
+    }
+    fetchMessage()
+  }, [])
+
+  return (
+    <>
+      <button onClick={() => getOpenTenderNotices()}>
+        Click here to get tender download
+      </button>
+      {/* <h1>{message}</h1> */}
+      {/* <p>{EmployeData.map((employee, index) => (
+    <div key={index}>  {employee.first_name}</div>
+      ))}
+      </p> */}
+    </>
   )
 }
 
