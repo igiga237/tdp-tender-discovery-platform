129cdc32876b94bc378a9018148828f952300627
fmarque <104608984+fmarque@users.noreply.github.com>
2025-03-04 21:56:19 -0500

    + inserted missing keys from TenderNoticeInterface as they were required for backend GET filtering task

--- apps/frontend/src/api.ts
+++ apps/frontend/src/api.ts
@@ -11,31 +11,35 @@ const PDF_ANALYSIS_URL = 'http://localhost:4500'
  * Interface defining the structure of a tender notice
  */
 export interface TenderNoticeInterface {
-  'title-titre-eng': string
-  'tenderStatus-appelOffresStatut-eng': string
-  'gsinDescription-nibsDescription-eng': string
-  'unspscDescription-eng': string
-  'noticeType-avisType-eng': string
-  'procurementMethod-methodeApprovisionnement-eng': string
-  'selectionCriteria-criteresSelection-eng': string
-  'limitedTenderingReason-raisonAppelOffresLimite-eng': string
-  'tradeAgreements-accordsCommerciaux-eng': string
-  'regionsOfOpportunity-regionAppelOffres-eng': string
-  'regionsOfDelivery-regionsLivraison-eng': string
-  'contractingEntityName-nomEntitContractante-eng': string
-  'contractingEntityAddressLine-ligneAdresseEntiteContractante-eng': string
-  'contractingEntityAddressCity-entiteContractanteAdresseVille-eng': string
-  'contractingEntityAddressProvince-entiteContractanteAdresseProvince-eng': string
-  'contractingEntityAddressCountry-entiteContractanteAdressePays-eng': string
-  'endUserEntitiesName-nomEntitesUtilisateurFinal-eng': string
-  'endUserEntitiesAddress-adresseEntitesUtilisateurFinal-eng': string
-  'contactInfoAddressLine-contactInfoAdresseLigne-eng': string
-  'contactInfoCity-contacterInfoVille-eng': string
-  'contactInfoProvince-contacterInfoProvince-eng': string
-  'contactInfoCountry-contactInfoPays-eng': string
-  'noticeURL-URLavis-eng': string
-  'attachment-piecesJointes-eng': string
-  'tenderDescription-descriptionAppelOffres-eng': string
+  'title-titre-eng': string;
+  'referenceNumber-numeroReference': string; // ADDED: Tender Reference ID
+  'tenderStatus-appelOffresStatut-eng': string;
+  'tenderClosingDate-appelOffresDateCloture': string; // ADDED: Closing Date
+  'expectedContractStartDate-dateDebutContratPrevue': string; // ADDED: Contract Start Date
+  'expectedContractEndDate-dateFinContratPrevue': string; // ADDED: Contract End Date
+  'gsinDescription-nibsDescription-eng': string;
+  'unspscDescription-eng': string;
+  'noticeType-avisType-eng': string;
+  'procurementMethod-methodeApprovisionnement-eng': string;
+  'selectionCriteria-criteresSelection-eng': string;
+  'limitedTenderingReason-raisonAppelOffresLimite-eng': string;
+  'tradeAgreements-accordsCommerciaux-eng': string;
+  'regionsOfOpportunity-regionAppelOffres-eng': string;
+  'regionsOfDelivery-regionsLivraison-eng': string;
+  'contractingEntityName-nomEntitContractante-eng': string;
+  'contractingEntityAddressLine-ligneAdresseEntiteContractante-eng': string;
+  'contractingEntityAddressCity-entiteContractanteAdresseVille-eng': string;
+  'contractingEntityAddressProvince-entiteContractanteAdresseProvince-eng': string;
+  'contractingEntityAddressCountry-entiteContractanteAdressePays-eng': string;
+  'endUserEntitiesName-nomEntitesUtilisateurFinal-eng': string;
+  'endUserEntitiesAddress-adresseEntitesUtilisateurFinal-eng': string;
+  'contactInfoAddressLine-contactInfoAdresseLigne-eng': string;
+  'contactInfoCity-contacterInfoVille-eng': string;
+  'contactInfoProvince-contacterInfoProvince-eng': string;
+  'contactInfoCountry-contactInfoPays-eng': string;
+  'noticeURL-URLavis-eng': string;
+  'attachment-piecesJointes-eng': string;
+  'tenderDescription-descriptionAppelOffres-eng': string;
 }
 
 /**
