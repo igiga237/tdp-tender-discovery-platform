493a04f8e14920b78d23971fdd959021e16fad18
Ansh Jindal <jindalansh498@gmail.com>
2025-03-07 17:17:15 -0500

    mergedJacobPR

--- apps/frontend/src/app/app.tsx
+++ apps/frontend/src/app/app.tsx
@@ -1,25 +1,67 @@
-import { Route, Routes, Link} from "react-router-dom"
-import TenderData from "./pages/TenderData"
-import LeadGenChatV2 from "./pages/LeadGenChatV2"
-import Rfp from "./pages/Rfp"
-export function App() {
+import { Route, Routes, Link, Outlet } from "react-router-dom";
+import TenderData from "./pages/TenderData";
+import LeadGenChatV2 from "./pages/LeadGenChatV2";
+import Rfp from "./pages/Rfp";
+import logo from "../assets/wouessi-new-logo.png";
 
+
+function Layout() {
   return (
-    <>
-      <nav className="flex flex-col gap-12 text-5xl font-bold">
-        <Link to="/tenderdata">Check out Tender Data</Link>
-        <Link to="/leadgenchatv2">
-          Go to prompt page to filter leads, generation, v2
-        </Link>
-        <Link to="rfp">Testing page for RFP</Link>
+    <div className="flex h-screen">
+      {/* Sidebar */}
+      <nav className="w-64 bg-gray-800 text-white p-5 space-y-4">
+        <img src={logo} alt="Wouessi Logo" className="w-32 h-auto" />
+        <ul className="space-y-3">
+          <li>
+            <Link to="/tenderdata" className="block p-3 bg-gray-700 rounded">
+              Tender Data
+            </Link>
+          </li>
+          <li>
+            <Link to="/leadgenchatv2" className="block p-3 bg-gray-700 rounded">
+              Lead Generation
+            </Link>
+          </li>
+          <li>
+            <Link to="/rfp" className="block p-3 bg-gray-700 rounded">
+              Capability Assessment
+            </Link>
+          </li>
+          <li>
+            <Link to="/rfp" className="block p-3 bg-gray-700 rounded">
+              Benchmarking
+            </Link>
+          </li>
+        </ul>
       </nav>
-      <Routes>
-        <Route path="/tenderdata" element={<TenderData />} />
-        <Route path="/leadgenchatv2" element={<LeadGenChatV2 />} />
-        <Route path="/rfp" element={<Rfp />} />
-      </Routes>
-    </>
-  )
+
+      {/* Main Content Area */}
+      <div className="flex-1 flex flex-col">
+        {/* Top Bar */}
+        <header className="bg-gray-900 text-white p-4 text-xl font-bold">
+          Tender Discovery Platform
+        </header>
+
+        {/* Page Content */}
+        <main className="p-6 flex-1 bg-gray-100 overflow-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export function App() {
+  return (
+    <Routes>
+      {/* Apply Layout to all pages */}
+      <Route path="/" element={<Layout />}>
+        <Route path="tenderdata" element={<TenderData />} />
+        <Route path="leadgenchatv2" element={<LeadGenChatV2 />} />
+        <Route path="rfp" element={<Rfp />} />
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
